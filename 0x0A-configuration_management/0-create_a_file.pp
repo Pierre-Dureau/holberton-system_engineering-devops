8eda3c1fa6d26877b351b47196aca4af646d020f
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-12 22:44:23 +0200

    create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # create a file in /tmp
 file { 'school':
-  'path'    => /tmp/school,
-  'mode'    => 0744,
-  'owner'   => www-data,
-  'group'   => www-data,
-  'content' => I love Puppet,
-  'ensure'  => present,
+  path => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
