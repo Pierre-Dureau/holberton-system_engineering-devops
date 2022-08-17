71a6ad00458e0ca1e98c6bf0b4615312b2744578
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-17 11:28:18 +0200

    Server configuration with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,20 +3,17 @@ package { 'nginx':
   ensure   => 'installed',
   provider => 'apt',
 }
-
 file { 'index.html':
   ensure  => 'present',
-  path    => '/var/www/html/index.html',
-  content => 'Hello World\n',
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World',
 }
-
 file_line { 'default':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => '\tlocation /redirect_me {\n\t\trewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\t}',
+  line   => 'location /redirect_me {rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;}',
 }
-
 service { 'nginx':
   ensure  => 'running',
   require => Package['nginx'],
