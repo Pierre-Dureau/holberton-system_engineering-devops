3c470a3227924a68d9ad25774a62c9424ef63272
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-22 11:55:08 +0200

    Server configuration with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,15 @@
+# Server configuration with puppet
+package { 'nginx':
+  ensure   => 'installed',
+  provider => 'apt',
+}
+file_line { 'default':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'add_header X-Served-By ${HOSTNAME};}',
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
