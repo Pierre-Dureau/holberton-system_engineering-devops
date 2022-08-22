dee1a0151ec7417c85ccc3ed478b54ecfe1175c2
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-22 13:15:28 +0200

    Server configuration with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,13 @@
 # Server configuration with puppet
+# an new ubuntu machine
+
 exec { 'update':
   command => '/usr/bin/apt-get update',
 }
 package { 'nginx':
-  ensure   => 'installed',
-  provider => 'apt',
+  ensure  => 'present',
+  name    => 'nginx',
+  require => Exec['update'],
 }
 file_line { 'default':
   ensure  => present,
