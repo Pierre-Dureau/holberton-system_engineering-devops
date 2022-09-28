7eb406d6c76dd3e737ef5855e8ca49da86d0540d
Pierre Dureau <dureaupierre53@gmail.com>
2022-09-28 10:58:17 +0200

    Add the task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # find out why Apache is returning a 500 error
-
-exec { 'change phpp to php':
-	command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	provider => shell,
+exec {'change phpp to php':
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  provider => shell,
 }
