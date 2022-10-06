223ab1af500df93bc5c1079738c90c9d02af6471
Pierre Dureau <dureaupierre53@gmail.com>
2022-10-06 11:15:35 +0200

    Add task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@ exec {'change ulimit 15 to 15000':
   command  => 'sed -i s/15/15000/g /etc/default/nginx',
   provider => shell,
 }
-exec {'restart nginx':
-  command  => 'sudo service nginx restart',
-  path     => '/usr/bin',
+service {'nginx':
+  ensure => 'running',
+  enable => true,
 }
